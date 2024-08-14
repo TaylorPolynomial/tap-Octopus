8fb743765ad40774939c1f0deddd84d6fa0fed79
damien <damienkharley@gmail.com>
2024-08-14 21:02:57 +0100

    first commit

--- tap-octopus/tap_octopus/streams.py
+++ tap-octopus/tap_octopus/streams.py
@@ -25,7 +25,7 @@ class ElectricStream(OctopusStream):
     """Define custom stream."""
 
     name = "electric"
-    path = "electricity-meter-points/1591011973471/meters/21E5385523/consumption"
+    path = "" # TODO: Enter electric consumption path
     # primary_keys: t.ClassVar[list[str]] = ["id"]
     replication_key = "interval_end"
     is_sorted = True 
@@ -52,7 +52,7 @@ class GasStream(OctopusStream):
     """Define custom stream."""
 
     name = "gas"
-    path = "gas-meter-points/1919231705/meters/E6E00207622020/consumption"
+    path = "" # TODO: Enter gas consumption path
     # primary_keys: t.ClassVar[list[str]] = ["id"]
     replication_key = "interval_end"
     is_sorted = True 
@@ -72,18 +72,4 @@ class GasStream(OctopusStream):
             "interval_end",
             th.DateTimeType
         ),
-    ).to_dict()
-
-
-class GroupsStream(OctopusStream):
-    """Define custom stream."""
-
-    name = "groups"
-    path = "/groups"
-    primary_keys: t.ClassVar[list[str]] = ["id"]
-    replication_key = "modified"
-    schema = th.PropertiesList(
-        th.Property("name", th.StringType),
-        th.Property("id", th.StringType),
-        th.Property("modified", th.DateTimeType),
     ).to_dict()
