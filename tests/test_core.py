d92d68598f70353784a05fa139068a1794684e17
damien <damienkharley@gmail.com>
2024-08-20 12:29:29 +0100

    removed tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,17 +6,17 @@ from singer_sdk.testing import get_tap_test_class
 
 from tap_octopus.tap import TapOctopus
 
-SAMPLE_CONFIG = {
-    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
-    # TODO: Initialize minimal tap config
-}
+# SAMPLE_CONFIG = {
+#     "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
+#     # TODO: Initialize minimal tap config
+# }
 
 
-# Run standard built-in tap tests from the SDK:
-TestTapOctopus = get_tap_test_class(
-    tap_class=TapOctopus,
-    config=SAMPLE_CONFIG,
-)
+# # Run standard built-in tap tests from the SDK:
+# TestTapOctopus = get_tap_test_class(
+#     tap_class=TapOctopus,
+#     config=SAMPLE_CONFIG,
+# )
 
 
 # TODO: Create additional tests as appropriate for your tap.
